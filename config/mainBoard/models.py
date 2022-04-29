bf64e691c4b1afa0e4a236b5505e87c991e6a79f
myeongjunkim <ilikerunning@naver.com>
2022-04-29 23:50:39 +0900

    feat: category class

--- config/mainBoard/models.py
+++ config/mainBoard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    view_auth = models.IntegerField()
+    
+    
+    
