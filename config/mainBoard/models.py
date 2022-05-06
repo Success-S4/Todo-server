0dfd7fbec79f48769d2fb6e9d6473520c1a64f58
myeongjunkim <ilikerunning@naver.com>
2022-05-06 13:47:22 +0900

    feat: category crud

--- config/mainBoard/models.py
+++ config/mainBoard/models.py
@@ -10,4 +10,8 @@ class Category(models.Model):
     view_auth = models.IntegerField()
     
     
-    
+class Todo(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
+    content = models.CharField(max_length = 50)
+    is_completed = models.BooleanField()
+    pup_date = models.DateTimeField(auto_now_add=True)
